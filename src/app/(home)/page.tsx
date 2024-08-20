0a991a629fe15d65a75568913900bda9efcbb1f6
Guilherme <guilhermeriguitti1997@gmail.com>
2024-08-20 00:05:34 -0300

    deploy testado

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,19 @@
-// import { NewsHighlightSlide } from "@/modulos/news/components/NewsHighlightSlide";
-// import { NewsHomeIni } from "@/modulos/news/components/NewsHomeIni";
-// import { NewsService } from "@/modulos/news/news.service";
+import { NewsHighlightSlide } from "@/modulos/news/components/NewsHighlightSlide";
+import { NewsHomeIni } from "@/modulos/news/components/NewsHomeIni";
+import { NewsService } from "@/modulos/news/news.service";
 
 export default async function HomePage() {
-    // const service = new NewsService();
-    // const highlights = await service.getHighlights(3);
+    const service = new NewsService();
+    const highlights = await service.getHighlights(3);
 
     return (
         <div className="grid grid-cols-7 p-10 bg-white gap-2">
             <div className="col-span-3">
 
-                {/* <NewsHighlightSlide highlights={highlights} /> */}
+                <NewsHighlightSlide highlights={highlights} />
             </div>
             <div className="col-span-1">
-                {/* <NewsHomeIni news={highlights} /> */}
+                <NewsHomeIni news={highlights} />
             </div>
 
         </div>
